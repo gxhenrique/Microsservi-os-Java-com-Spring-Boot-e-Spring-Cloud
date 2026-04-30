ce33be07fceb2b21e85b30fdb8645a395722abca
gxhenrique <gxhenriquecma2022@gmail.com>
2026-04-30 17:14:33 -0300

    login, jwt token

--- hr-oauth/src/main/java/com/example/hroauth/services/UserServiceClient.java
+++ hr-oauth/src/main/java/com/example/hroauth/services/UserServiceClient.java
@@ -6,13 +6,16 @@ package com.example.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.hroauth.entities.User;
 import com.example.hroauth.userFeingClients.UserFeingClient;
 
 @Service
-public class UserServiceClient {
+public class UserServiceClient implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserServiceClient.class);
 	
@@ -29,6 +32,19 @@ public class UserServiceClient {
 		}
 		
 		logger.info("Email: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userFeingClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		logger.info("Email: " + username);
 		return user;
 	}
 }

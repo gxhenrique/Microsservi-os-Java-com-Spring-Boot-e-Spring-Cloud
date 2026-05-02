f6704a86603a3d68264f4b1aab3ebacb1dd17b09
gxhenrique <gxhenriquecma2022@gmail.com>
2026-05-02 17:34:44 -0300

    segurity

--- hr-oauth/src/main/java/com/example/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/example/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 	
+	@Value(value = "${jwt.scret}")
+	private String jwtSecret;
+	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -19,7 +23,7 @@ public class AppConfig {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		tokenConverter.setSigningKey(jwtSecret);
 		
 		return tokenConverter;
 		
